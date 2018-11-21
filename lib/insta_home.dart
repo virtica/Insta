b9196abdb54b1e671fc6e5e943b7b85900d02919
Kenneth Watkins <kenneth@virtica.net>
2018-11-21 15:52:18 -0600

    Updated Insta list file

--- lib/insta_home.dart
+++ lib/insta_home.dart
@@ -1,20 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:insta/insta_body.dart';
 
-
 class InstaHome extends StatelessWidget {
-
   final topBar = new AppBar(
-
-
     backgroundColor: new Color(0xfff8faf8),
     centerTitle: true,
     elevation: 1.0,
     leading: new Icon(Icons.camera_alt),
     title: SizedBox(
-      height: 35.0,
-      child: Image.asset("assets/images/insta_logo.png")
-    ),
+        height: 35.0, child: Image.asset("assets/images/insta_logo.png")),
     actions: <Widget>[
       Padding(
         padding: const EdgeInsets.only(right: 12.0),
@@ -23,38 +17,34 @@ class InstaHome extends StatelessWidget {
     ],
   );
 
-
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: topBar,
-        // ignore: missing_identifier
-        body:
-          new InstaBody(),
-
+      body: new InstaBody(),
       bottomNavigationBar: new Container(
         height: 60.0,
         alignment: Alignment.center,
         color: Colors.white,
         child: new BottomAppBar(
-          child: new Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: <Widget>[
-              new IconButton(icon: Icon(Icons.home), onPressed: (){}), // Home Button
-              new IconButton(icon: Icon(Icons.search), onPressed: (){}), // Search Button
-              new IconButton(icon: Icon(Icons.add_box), onPressed: (){}), // Add Button
-              new IconButton(icon: Icon(Icons.favorite), onPressed: (){}), // Favorites Button
-              new IconButton(icon: Icon(Icons.account_circle), onPressed: (){}), // Profile Button
-            ],
-          )
-        ),
+            child: new Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            new IconButton(
+                icon: Icon(Icons.home), onPressed: () {}), // Home Button
+            new IconButton(
+                icon: Icon(Icons.search), onPressed: () {}), // Search Button
+            new IconButton(
+                icon: Icon(Icons.add_box), onPressed: () {}), // Add Button
+            new IconButton(
+                icon: Icon(Icons.favorite),
+                onPressed: () {}), // Favorites Button
+            new IconButton(
+                icon: Icon(Icons.account_circle),
+                onPressed: () {}), // Profile Button
+          ],
+        )),
       ),
     );
   }
-
-
 }
-
-
-
-
--- lib/insta_list.dart
+++ lib/insta_list.dart
@@ -1,40 +1,69 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class InstaList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var deviceSize = MediaQuery.of(context).size;
     return ListView.builder(
-        itemCount: 5,
-        itemBuilder: (context, index) => Column(
-          children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.fromLTRB(16.0, 16.0, 8.0, 16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  new Container(
-                    height: 40.0,
-                    width: 40.0,
-                    decoration: new BoxDecoration(
-                      shape: BoxShape.circle,
-                      image: new DecorationImage(fit: BoxFit.fill,
-                      image: new NetworkImage("https://scontent-ort2-2.cdninstagram.com/vp/3747d74c1b7a2ae14d3c9ebcae952c2a/5C8B4B16/t51.2885-19/s150x150/17076095_1498345800176937_6360962001212014592_a.jpg")),
+      itemCount: 5,
+      itemBuilder: (context, index) => Column(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.fromLTRB(16.0, 16.0, 8.0, 16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    new Container(
+                      height: 40.0,
+                      width: 40.0,
+                      decoration: new BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: new DecorationImage(
+                            fit: BoxFit.fill,
+                            image: new NetworkImage(
+                                "https://scontent-ort2-2.cdninstagram.com/vp/3747d74c1b7a2ae14d3c9ebcae952c2a/5C8B4B16/t51.2885-19/s150x150/17076095_1498345800176937_6360962001212014592_a.jpg")),
+                      ),
                     ),
-                  ),
-                  new SizedBox(
-                    width: 10.0
-                  ),
-                  new Text(
-                    "blahblahblah",
-                    style: TextStyle(fontWeight: FontWeight.bold),
-                  )
-                ],
+                    new SizedBox(width: 10.0),
+                    new Text(
+                      "blahblahblah",
+                      style: TextStyle(fontWeight: FontWeight.bold),
+                    ),
+                    new IconButton(
+                        icon: Icon(Icons.more_horiz), onPressed: null)
+                  ],
+                ),
               ),
-            )
-          ],
-        ))
-
+              Flexible(
+                fit: FlexFit.loose,
+                child: new Image.network(
+                    "https://images.pexels.com/photos/1569012/pexels-photo-1569012.jpeg",
+                    fit: BoxFit.cover),
+              ),
+              Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    new Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          new Icon(
+                            FontAwesomeIcons.heart,
+                          ),
+                          new SizedBox(width: 16.0),
+                          new Icon(FontAwesomeIcons.comment),
+                          new SizedBox(
+                            width: 16.0,
+                          ),
+                          new Icon(FontAwesomeIcons.paperPlane)
+                        ])
+                  ],
+                ),
+              )
+            ],
+          ),
+    );
   }
-
 }
